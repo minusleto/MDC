e02f328b1c3ec2b6ea180a5032fb366ec3dd1c81
minusleto <72198787+minusleto@users.noreply.github.com>
2026-09-10 20:29:07 +0500

    Add Military effects to sidebar

--- astro.config.mjs
+++ astro.config.mjs
@@ -32,6 +32,7 @@ export default defineConfig({
 						{ label: 'Постройки', translations: { en: 'Buildings' }, slug: 'code-resource/effects/buildings' },
 						{ label: 'Экономика', translations: { en: 'Economy' }, slug: 'code-resource/effects/economy' },
 						{ label: 'Технологии', translations: { en: 'Technology' }, slug: 'code-resource/effects/technology' },
+						{ label: 'Военные', translations: { en: 'Military' }, slug: 'code-resource/effects/military' },
 						{ label: 'Внутренние фракции', translations: { en: 'Internal Factions' }, slug: 'code-resource/effects/internal-factions' },
 						{ label: 'Иностранное влияние', translations: { en: 'Foreign Influence' }, slug: 'code-resource/effects/influence' },
 						{ label: 'Политика', translations: { en: 'Politics' }, slug: 'code-resource/effects/politics' },
